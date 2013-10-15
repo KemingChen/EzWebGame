c72ef130698d7447f24efe00e554ec15f5dc06c2
gary62107 <gary62107@gmail.com>
2013-10-15 16:20:16 +0800

    index 切版

--- application/views/header.php
+++ application/views/header.php
@@ -7,7 +7,7 @@
     <meta name="author" content="keming&ZhongTing" />
     <link rel="shortcut icon" href="<?=base_url("assets/icon/favicon.ico")?>" />
 
-    <title>Narrow Jumbotron Template for Bootstrap</title>
+    <title>EzWebGames</title>
 
     <!-- Bootstrap core CSS -->
     <link href="<?=base_url("assets/css/bootstrap.css")?>" rel="stylesheet" />
@@ -28,39 +28,8 @@
       <div class="header">
         <ul class="nav nav-pills pull-right">
           <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="#">Developer</a></li>
+          <li><a href="#">Member</a></li>
         </ul>
-        <h3 class="text-muted">Project name</h3>
-      </div>
-
-      <div class="jumbotron">
-        <h1>Jumbotron heading</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-lg btn-success" href="#">Sign up today</a></p>
-      </div>
-
-      <div class="row marketing">
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-      </div>
-      
+        <h3 class="text-muted">EzWebGame</h3>
+      </div>      
---
+++ application/views/index.php
@@ -0,0 +1,18 @@
+   <div class="jumbotron">
+        <h1>EzWebGame</h1>
+        <p class="lead">Enjoy Games in EzWebGame</p>
+        <p><a class="btn btn-lg btn-success" href="#">Sign up today</a></p>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+        </div>
+
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+        </div>
+      </div>
